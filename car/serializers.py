8d3dcbdd5734de9db1ebdf2fb35006b324f723c5
VikTOR <dev.psychologist@gmail.com>
2025-03-08 19:45:43 +0200

    Create serializer

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,25 @@
 from rest_framework import serializers
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[serializers.MinValueValidator(1),
+                    serializers.MaxValueValidator(1914)]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(
+        required=False,
+        allow_null=True,
+        allow_blank=True)
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import json
+from car.serializers import CarSerializer
 from car.models import Car
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serialized_car = CarSerializer(car)
+    return json.dumps(serialized_car).encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(car_json: bytes) -> Car:
+    data = json.loads(car_json.decode("utf-8"))
+    serialized_car = CarSerializer(data=data)
+    serialized_car.is_valid(raise_exception=True)
+    return Car(**serialized_car.validated_data)
